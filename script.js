7d0f0ad49b6588ff1f784dddd09658b61182de2a
Benson Njuguna <njugunab655@gmail.com>
2022-09-13 16:01:09 +0300

    Create a 16*16 grid using JavaScript

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ function makeGrid(rows, columns) {
     for(let c = 0; c < rows * columns; c++){
         const cell = document.createElement('div')
         cell.style.border = '1px solid black'
-        cell.innerText = `C${c + 1}`
         gridContainer.appendChild(cell)
     }
 }
