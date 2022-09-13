0f89c5b3e9aca3f0dc7339ed7ee6f28093d5adb8
Benson Njuguna <njugunab655@gmail.com>
2022-09-13 16:15:45 +0300

    Add a hover effect using JavaScript

--- script.js
+++ script.js
@@ -3,8 +3,15 @@ function makeGrid(rows, columns) {
     for(let c = 0; c < rows * columns; c++){
         const cell = document.createElement('div')
         cell.style.border = '1px solid black'
+        cell.classList.add('cell')
         gridContainer.appendChild(cell)
     }
 }
 
 makeGrid(16, 16)
+
+function changeColor(e) {
+    e.target.style.backgroundColor = "gray"
+}
+const cells = document.querySelectorAll('div.cell')
+cells.forEach(cell => cell.addEventListener('mouseover', changeColor))
